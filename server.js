c460fd33b5b198c717e6b7179474ba1035f9cf3a
Gideon Rosales <gideonrosales@gmail.com>
2014-08-17 10:50:10 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var net = require('net');
+var Cassandra = require( 'cassandra-client').Connection;
+var con = new Cassandra( { host: '127.0.0.1', port: 9160, keyspace: 'node_server' } );
+con.connect(function(err) { console.log ( 'Cassandra deployed' ); });
+var uuid = require ( 'node-uuid' );
+var Controller = require( './lib/controller' );
+var controller = new Controller( { db : con, uuid: uuid } );
+
+var server = net.createServer( function ( socket ) {
+	controller.setSocket( socket );
+} );
+
+server.listen(1000, function() {
+	console.log('Logging server initiated');
+} );
